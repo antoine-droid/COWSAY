4bdb20f95a0464849d4829614af14fff026fde5e
Debord <debordantoine0@gmail.com>
2022-11-29 20:31:52 +0100

    Talking cow

---
+++ index.js
@@ -0,0 +1,19 @@
+const infos = require('./informations');
+console.log(`Hi, my name is ${infos.info} and I'm in ${infos.campus} campus`);
+
+const cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "Hi, I'm Antoine and I'm in remote ",
+    e : "oO",
+    T : "U "
+}));
+
+
+
+
+
+
+
+
+
